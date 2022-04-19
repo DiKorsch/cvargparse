03497c1563406a3de822a34fc0ad2fec459a36b1
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2022-04-19 14:35:19 +0200

    added some convenience class methods

--- cvargparse/argument.py
+++ cvargparse/argument.py
@@ -5,6 +5,20 @@ class Argument(object):
 		self.args = args # positional arugments
 		self.kw = kw # keyword arguments
 
+	@classmethod
+	def int(cls, *args, **kw):
+		arg_type = kw.pop("type", int)
+		return cls(*args, type=arg_type, **kw)
+
+	@classmethod
+	def float(cls, *args, **kw):
+		arg_type = kw.pop("type", float)
+		return cls(*args, type=arg_type, **kw)
+
+	@classmethod
+	def flag(cls, *args, **kw):
+		action = kw.pop("action", "store_true")
+		return cls(*args, action=action, **kw)
 
 class FileArgument(Argument):
 	def __init__(self, *args, **kw):
