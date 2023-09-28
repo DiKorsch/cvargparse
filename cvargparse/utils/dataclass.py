cb9f9e93b4cdfbd9550d81a931181c8b05a3a155
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2023-09-28 10:25:03 +0200

    forgot to update the usage of pyaml

--- cvargparse/utils/dataclass.py
+++ cvargparse/utils/dataclass.py
@@ -1,4 +1,4 @@
-import pyaml
+import yaml
 
 from dataclasses import Field
 from dataclasses import MISSING
@@ -48,7 +48,7 @@ def cvdataclass(cls=None, *args, repr=False, **kwargs):
 
 	def _yaml_repr_(self) -> str:
 		cls_name = type(self).__name__
-		return pyaml.dump({cls_name: self.__dict__}, sort_dicts=False)
+		return yaml.dump({cls_name: self.__dict__}, sort_dicts=False)
 
 	def wrap(cls):
 		if not repr:
